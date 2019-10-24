486b5fe9c0597d40f2a5187c60e5d9bee8901a6d
ThomasD972 <thomas.delage972@gmail.com>
2019-10-24 21:04:16 +0200

    Terminé

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+By Thomas Delage  To run the db :
+
+$bundle install to install ALL the gems
+$rails db seed to initialize a database
+$rails console to open the console ($rcif you're a hackerman)
+Try $tp Dog.all, $tp Dogsitter.all, $tp Stroll.all to check if everything works (it does)(well it did yesterday at least)
---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker' 
+Stroll.destroy_all
+Dog.destroy_all
+Dogsitter.destroy_all
+City.destroy_all
+
+cities = []
+10.times do
+    cities << City.create(
+    name: Faker::Movies::LordOfTheRings.location
+    )
+end
+
+dogsitters = []
+25.times do
+    dogsitters << Dogsitter.create(
+        name: Faker::Ancient.hero, 
+      age: Faker::Number.between(from: 15, to: 99),
+      city: cities.sample
+  )
+end
+dogs = []
+100.times do
+    dogs << Dog.create(
+        name: Faker::Games::Heroes.name,
+        city: cities.sample
+    )
+  end
+  
+  150.times do
+    Stroll.create(
+        date: Faker::Time.forward(days: 100, period: :day),
+        dogsitter: dogsitters.sample,
+        dog: dogs.sample,
+        city: cities.sample
+    )
+  end
